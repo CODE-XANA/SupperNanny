38b590083c024ac26af51263cf3c3322c84fd519
Tanguy <114865142+tanguytasuko@users.noreply.github.com>
2025-04-25 11:14:35 +0200

    Update full_script.sql with admins table

--- rust-landlock-main/queries_sql/full_script.sql
+++ rust-landlock-main/queries_sql/full_script.sql
@@ -16,6 +16,9 @@ DROP TABLE IF EXISTS roles                   CASCADE;
 DROP TABLE IF EXISTS users                   CASCADE;
 DROP TABLE IF EXISTS default_policies        CASCADE;
 DROP TABLE IF EXISTS security_logs           CASCADE;
+DROP TABLE IF EXISTS user_admin              CASCADE;
+DROP TABLE IF EXISTS permission_admin        CASCADE;
+DROP TABLE IF EXISTS role_permissions_admin  CASCADE;
 DROP TYPE  IF EXISTS policy_status           CASCADE;
 DROP INDEX IF EXISTS unique_app_role_pending;
 
@@ -65,6 +68,26 @@ CREATE TABLE app_policy (
     updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
     UNIQUE (app_name, role_id),
     FOREIGN KEY (role_id) REFERENCES roles(role_id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE user_admin (
+  user_admin_id SERIAL PRIMARY KEY,
+  username_admin VARCHAR(255) NOT NULL UNIQUE,
+  password_hash_admin VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE permission_admin (
+  permission_admin_id SERIAL PRIMARY KEY,
+  permission_admin_name VARCHAR(255) NOT NULL UNIQUE 
+);
+
+CREATE TABLE role_permissions_admin (
+  user_admin_id INTEGER NOT NULL,
+  permission_admin_id INTEGER NOT NULL,
+  PRIMARY KEY (user_admin_id, permission_admin_id),
+  FOREIGN KEY (user_admin_id) REFERENCES user_admin(user_admin_id) ON DELETE CASCADE,
+  FOREIGN KEY (permission_admin_id) REFERENCES permission_admin(permission_admin_id) ON DELETE CASCADE
 );
 
 -- ENUM for request status
@@ -135,32 +158,49 @@ CREATE TABLE security_logs (
 
 -- ---------- SEED DATA ------------------------------------
 INSERT INTO roles (role_id, role_name) VALUES
-  (1, 'developer_senior'),
+  (1, 'admin'),
   (2, 'developer'),
-  (3, 'user'),
-  (4, 'reviewer');
+  (3, 'user');
 
 INSERT INTO permissions (permission_id, permission_name) VALUES
-  (1, 'manage_policies'),
-  (2, 'view_events'),
-  (3, 'execute_apps'),
-  (4, 'view_policies'),
-  (5, 'approve_policies');
+  (1, 'manage_rules'),
+  (2, 'manage_users'),
+  (3, 'manage_policies'),
+  (4, 'view_events'),
+  (5, 'execute_apps');
+
 
 INSERT INTO role_permissions (role_id, permission_id) VALUES
   (1, 1), (1, 2), (1, 3), (1, 4), (1, 5),
   (2, 2), (2, 3),
-  (3, 3),
-  (4, 4);
+  (3, 3);
 
 INSERT INTO users (user_id, username, password_hash) VALUES
-  (1, 'developer_senior',     '$2a$12$Gp.L8taKXJqc/N/T40fbyekjONH1PaioOfDcvHkubYVDOurNXRoPi'),
-  (2, 'developer', '$2a$12$OtLIa4HYtsp3nnbx4zQYjOpID.eohUtJYbf5Vu.tgS/hZZ0XLgEVe'),
-  (3, 'regular',   '$2a$12$tfr8QKoe8jy66nlHXOIlMeAHfxn5vj7inaLuBco3eiDmAJJVEDdBy'),
-  (4, 'reviewer',  '$2a$12$uSLo1q6uaGrXXdkBRxbTy.ugy1nW7Q2uhXtnZXV9AviPa8kcWGbF.');
+  (1, 'admin', '$2a$12$Gp.L8taKXJqc/N/T40fbyekjONH1PaioOfDcvHkubYVDOurNXRoPi'),
+  (2, 'developer','$2a$12$OtLIa4HYtsp3nnbx4zQYjOpID.eohUtJYbf5Vu.tgS/hZZ0XLgEVe'),
+  (3, 'regular', '$2a$12$tfr8QKoe8jy66nlHXOIlMeAHfxn5vj7inaLuBco3eiDmAJJVEDdBy'),
+  (4, 'reviewer', '$2a$12$uSLo1q6uaGrXXdkBRxbTy.ugy1nW7Q2uhXtnZXV9AviPa8kcWGbF.');
 
 INSERT INTO user_roles (user_id, role_id) VALUES
   (1, 1), (2, 2), (3, 3), (4, 1);
+
+INSERT INTO user_admin (user_admin_id, username_admin, password_hash_admin) VALUES
+  (1, 'admin_rules', '$argon2id$v=19$m=19456,t=2,p=1$e3gpoTCPNwVJQYGTXTd76w$FPS4FZSOeuD+PNAuriJjXcVZreDb01NvbfEw9cUHklY'),
+  (2, 'admin_users', '$argon2id$v=19$m=19456,t=2,p=1$OJqOCTakCBs/zJB8Cyutww$AzrtzQXoyHX5ghpuCN1or1e6l59gLuKKpnhLhdVAEGM'),
+  (3, 'admin_roles', '$argon2id$v=19$m=19456,t=2,p=1$AT0LbYjEISg+MzfWfVde7g$4zM6ieWnemMNXP06kZKuZ69O4Fo90uJaDLFZQ9xRbak'),
+  (4, 'admin_events', '$argon2id$v=19$m=19456,t=2,p=1$mB/qoTOgPDjRQP9uD72NLA$aO4FKrit2ulzTP1bXqIQ2hUOUAVMF8TAi9JZvEqUwr8');
+
+INSERT INTO permission_admin (permission_admin_id, permission_admin_name) VALUES
+  (1, 'manage_rules'),
+  (2, 'manage_users'),
+  (3, 'manage_roles'),
+  (4, 'view_events');
+
+  INSERT INTO role_permissions_admin (user_admin_id, permission_admin_id) VALUES
+  (1, 1),
+  (2, 2),
+  (3, 3),
+  (4, 4);
 
 INSERT INTO default_policies (
   role_id, default_ro, default_rw, tcp_bind, tcp_connect, allowed_ips, allowed_domains
